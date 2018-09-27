0d2f506b4f5c46a5bc8553bc5c3c2d1589cf7739
51rL1N5 <lipeols@hotmail.com>
2018-09-27 20:41:20 -0300

    Update -> Tudo implementado

---
+++ Makefile
@@ -0,0 +1,8 @@
+CC    =    g++
+CXX = g++
+
+all:    ga
+ga:    ga.h    ga.cpp
+
+clean:
+    rm -f *.o    ga
--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/*
+  Authors:
+            - Felipe Oliveira Lins e Silva
+            - Luis Gabriel Pereira Condados
+*/
+
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
@@ -5,38 +11,30 @@
 #include <vector>
 #include <ctime>
 #include <algorithm>
+#include <queue>
 
 ////// Define Utils
-
-#define TAM_POP 5000
-#define LIM_IT 10000
-
-// chance de mutação = MUT_CHANCE
-#define MUT_CHANCE 1
-
+#define TAM_POP 50000
+#define MAX_GERACOES 10000
+#define MUT_CHANCE 0.02L
 ///////////////////
-
-
 
 
 using namespace std;
 
-void roleta(int & pai, int & mae)
-{
-  pai = rand()%TAM_POP;
-  mae = rand()%TAM_POP;
-}
+void fitness(DNA* a,  const string& objetivo);
+void crossoverEMutacao(int & pai, int & mae, queue<DNA*> &newPop, vector<DNA*> & populacao, int & tam_objetivo);
 
 
 int main(int argc, char** argv)
 {
   srand(time(NULL));
 
-  double mut_c = 0.02;
+
   vector<DNA*> populacao;
   int totalFit = 0 ;
 
-  string objetivo(argv[1]);
+  string objetivo("HIGAR");
   int tam_objetivo = objetivo.length();
 
   // inicializar populacao
@@ -52,67 +50,83 @@ int main(int argc, char** argv)
 
   int it = 0;
 
-  while(it < LIM_IT)
+  while(it < MAX_GERACOES)
   {
     // SELECAO
     totalFit = 0;
 
     for(int i = 0; i < TAM_POP; i++){
       populacao.at(i)->fit = 0;
-      for(int j = 0; j < tam_objetivo; j++){
-        if(populacao.at(i)->gene.at(j) == objetivo.at(j)){
-          populacao.at(i)->fit += 10;
-        }
-      }
+      fitness(populacao.at(i),objetivo);
+
       if(populacao.at(i)->fit == tam_objetivo * 10) {
         cout << "PALAVRA ENCONTRADA NA GERACAO: " << it << endl;
+        cout << "PALAVRA: " << populacao.at(i)->gene << endl;
         return 0;
       }
       totalFit += populacao.at(i)->fit;
     }
 
     // ORDENAR PELO FIT
-
-    sort(populacao.begin(), populacao.end(), [](DNA * a, DNA *b) {
+    sort(populacao.begin(), populacao.end(), [](DNA *& a, DNA *& b) {
       return a->fit > b->fit;
     });
 
-    if(it %10 == 0)
-    cout << it << " MELHOR ORGANISMO DA GERACAO: " << populacao.at(0)->gene << endl;
+    cout << "GERACAO: " << it << " | MELHOR ORGANISMO DA GERACAO: " << populacao.at(0)->gene << endl;
 
-    // ROLETA
-/*    vector<double> probabilidade_roleta(TAM_POP + 1, 0);
-
-    for(int i = 1; i <= TAM_POP; i++) {
-      probabilidade_roleta[i] = ((double)populacao.at(i-1)->fit/(double)totalFit) + probabilidade_roleta[i-1];
-    }
-*/
-
+    // nova populacao
+    queue<DNA*> newPop;
 
     // CROSSOVER
-    for(int i = 0; i < TAM_POP; i++){
+    int pai = 0; // MELHOR FIT
 
-      int pai = -1, mae = -1;
-
-      roleta(pai, mae);
-
-      string auxPai, auxMae;
-
-      for(int j = 0; j < tam_objetivo/2; j++) {
-        (((double) rand()/ (RAND_MAX)) <= mut_c)? auxPai += rand()%96 + 32 : auxPai += populacao.at(pai)->gene.at(j);
-        (((double) rand()/ (RAND_MAX)) <= mut_c)? auxMae += rand()%96 + 32 : auxMae += populacao.at(pai)->gene.at(j);
-      }
-      for(int j = tam_objetivo/2; j < tam_objetivo; j++) {
-        (((double) rand()/ (RAND_MAX)) <= mut_c)? auxPai += rand()%96 + 32 : auxPai += populacao.at(mae)->gene.at(j);
-        (((double) rand()/ (RAND_MAX)) <= mut_c)? auxMae += rand()%96 + 32 : auxMae += populacao.at(mae)->gene.at(j);
-      }
-
-      populacao.at(pai)->gene = auxPai;
-      populacao.at(mae)->gene = auxMae;
+    for(int i = 0; i < TAM_POP/2; i++){
+      int mae = rand() % TAM_POP;
+      crossoverEMutacao(pai, mae, newPop, populacao, tam_objetivo);
     }
+
+    // renovar populacao
+    for(int i = 0; i < TAM_POP; i++) {
+      delete populacao[i];
+    }
+    for(int i = 0; i < TAM_POP; i++) {
+      populacao[i] = newPop.front();
+      newPop.pop();
+    }
+
 
     it++;
   }
 
   return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void fitness(DNA* a,  const string& objetivo){
+  for(uint i = 0; i < objetivo.length(); i++)
+    if(a->gene.at(i) == objetivo.at(i))
+      a->fit += 10;
+}
+
+void crossoverEMutacao(int & pai, int & mae, queue<DNA*> &newPop, vector<DNA*> & populacao, int & tam_objetivo)
+{
+  string auxPai, auxMae;
+
+  for(int j = 0; j < tam_objetivo/2; j++) {
+    (((double) rand()/ (RAND_MAX)) <= MUT_CHANCE)? auxPai += rand()%96 + 32 : auxPai += populacao.at(pai)->gene.at(j);
+    (((double) rand()/ (RAND_MAX)) <= MUT_CHANCE)? auxMae += rand()%96 + 32 : auxMae += populacao.at(pai)->gene.at(j);
+  }
+  for(int j = tam_objetivo/2; j < tam_objetivo; j++) {
+    (((double) rand()/ (RAND_MAX)) <= MUT_CHANCE)? auxPai += rand()%96 + 32 : auxPai += populacao.at(mae)->gene.at(j);
+    (((double) rand()/ (RAND_MAX)) <= MUT_CHANCE)? auxMae += rand()%96 + 32 : auxMae += populacao.at(mae)->gene.at(j);
+  }
+
+  DNA* tmp1 = new DNA();
+  DNA* tmp2 = new DNA();
+
+  tmp1->gene = auxPai;
+  tmp2->gene = auxMae;
+
+  newPop.push(tmp1);
+  newPop.push(tmp2);
 }
